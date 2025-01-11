9dd9479fe3cfd4bf0b3c322156dd771de1ecd88b
KevinGlad <kglad@wbteam.net>
2025-01-11 16:38:43 -0700

    return dir

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(
-    path.join(__dirname, "../spa/build/index.html")
-  );
+  // res.sendFile(
+  //   path.join(__dirname, "../spa/build/index.html")
+  // );
+  res.json({dir,__dirname})
 });
 
 module.exports = router;
