30148915101ad84b864b100e7c6c0de3362a09ad
KevinGlad <kglad@wbteam.net>
2025-01-11 22:55:54 -0700

    added consol.log

--- routes/vacuum.js
+++ routes/vacuum.js
@@ -2,19 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  let side = req.query.side
-  let id = req.query.id
-    console.log(req.body)
-  res.send({side, id, status: req.app.locals.vacuumStatus[side][id]})
+    let side = req.query.side
+    let id = req.query.id
+    console.log({side, id})
+    console.log(req.app.locals.vacuumStatus)
+    res.send({side, id, status: req.app.locals.vacuumStatus[side][id]})
 
 });
 
 router.post('/',function(req, res, next){
-  let {side,id} = req.body
+    let {side,id} = req.body
 
-  req.app.locals.vacuumStatus[side][id] = !req.app.locals.vacuumStatus[side][id]
+    req.app.locals.vacuumStatus[side][id] = !req.app.locals.vacuumStatus[side][id]
 
-  res.send({side, id, status: req.app.locals.vacuumStatus[side][id]})
+    res.send({side, id, status: req.app.locals.vacuumStatus[side][id]})
 })
 
 module.exports = router;
