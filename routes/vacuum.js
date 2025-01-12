c4d470d07d3f7071c74566809526bd8620b92185
KevinGlad <kglad@wbteam.net>
2025-01-11 23:24:00 -0700

    fixed headers for post

--- routes/vacuum.js
+++ routes/vacuum.js
@@ -4,17 +4,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     let side = req.query.side
     let id = req.query.id
-    console.log({side, id})
-    console.log(req.app.locals.vacuumStatus)
+
     res.send({side, id, status: req.app.locals.vacuumStatus[side][id]})
 
 });
 
 router.post('/',function(req, res, next){
     let {side,id} = req.body
-    console.log({side, id})
+
     req.app.locals.vacuumStatus[side][id] = !req.app.locals.vacuumStatus[side][id]
-    console.log(req.app.locals.vacuumStatus)
+
     res.send({side, id, status: req.app.locals.vacuumStatus[side][id]})
 })
 
