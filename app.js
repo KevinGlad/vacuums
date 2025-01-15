4359a4c662b268e519104a37d3488822df4fe2db
KevinGlad <kglad@wbteam.net>
2025-01-15 10:40:48 -0700

    fixed dashboard object

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use(express.static(path.join(__dirname, 'spa/build')))
 
 app.use("/", indexRouter)
 app.use("/vacuum",vacuumRouter)
-app.use("/store", storeRouter)
+app.use("/dashboard", storeRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/store.js
+++ routes/store.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     let store = req.query.store
 
-    res.send({store: req.app.locals.vacuumStatus[store]})
+    res.send({store, vacuums:req.app.locals.vacuumStatus[store]})
 
 });
 
