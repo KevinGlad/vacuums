400806496de649e739bf2edc79c2550561b94c08
KevinGlad <kglad@wbteam.net>
2025-01-29 09:44:50 -0700

    changed dashboard to stream

--- routes/store.js
+++ routes/store.js
@@ -3,8 +3,23 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     let store = req.query.store
+    //https://stackoverflow.com/questions/34657222/how-to-use-server-sent-events-in-express-js
+    res.writeHead(200, {
+        "Connection": "keep-alive",
+        "Cache-Control": "no-cache",
+        "Content-Type": "text/event-stream",
+        });
 
-    res.send({store, vacuums:req.app.locals.vacuumStatus[store]})
+        const interval = setInterval(() => {
+            const chunk = JSON.stringify({store, vacuums:req.app.locals.vacuumStatus[store]})
+            res.write(`data: ${chunk}\n\n`)
+        }, 100);
+
+        res.on("close", () => {
+            clearInterval(interval);
+            res.end();
+        });
+    //res.send({store, vacuums:req.app.locals.vacuumStatus[store]})
 
 });
 
